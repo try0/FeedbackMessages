fe6289ba38531852948fc9c785fdef77fab927de
Ryo Tsunoda <try0.development@gmail.com>
2020-04-20 22:47:55 +0900

    Add filter tests.

--- Tests/FeedbackMessages.AspNetCore.Test/FeedbackMessageActionFilterUnitTest.cs
+++ Tests/FeedbackMessages.AspNetCore.Test/FeedbackMessageActionFilterUnitTest.cs
@@ -44,5 +44,41 @@ namespace FeedbackMessages.AspNetCore.Test
             Assert.AreEqual(flashedStore.Count, 1);
         }
 
+        [TestMethod]
+        public void TestFilterPageHandlerExecuting()
+        {
+            var httpContext = InitializeHttpContext();
+
+            var store = new FeedbackMessageStore();
+            store.AddMessage(FeedbackMessage.Info("test"));
+            httpContext.Session.SetStore(FeedbackMessageStoreHolder.ITEM_KEY, store);
+
+            // loads message store from session
+            var filter = new FeedbackMessageActionFilter();
+            filter.OnPageHandlerExecuting(null);
+
+            var loadedStore = httpContext.Items[FeedbackMessageStoreHolder.ITEM_KEY] as FeedbackMessageStore;
+            Assert.IsNotNull(loadedStore);
+            Assert.AreEqual(loadedStore.Count, 1);
+
+        }
+
+        [TestMethod]
+        public void TestFilterPageHandlerExecuted()
+        {
+            var httpContext = InitializeHttpContext();
+
+            var store = new FeedbackMessageStore();
+            store.AddMessage(FeedbackMessage.Info("test"));
+            httpContext.Items[FeedbackMessageStoreHolder.ITEM_KEY] = store;
+
+            // flashs message store to session
+            var filter = new FeedbackMessageActionFilter();
+            filter.OnPageHandlerExecuted(null);
+
+            var flashedStore = httpContext.Session.GetStore(FeedbackMessageStoreHolder.ITEM_KEY);
+            Assert.IsNotNull(flashedStore);
+            Assert.AreEqual(flashedStore.Count, 1);
+        }
     }
 }
