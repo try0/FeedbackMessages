59231f47139bff4efb4bb6159642594aa3bd9a18
Ryo Tsunoda <try0.development@gmail.com>
2020-04-21 19:34:17 +0900

    Add message converter test.

---
+++ Tests/FeedbackMessages.Test/FeedbackMessageConverterUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeedbackMessages.Test
+{
+    [TestClass]
+    public class FeedbackMessageConverterUnitTest : FeedbackMessagesUnitTestBase
+    {
+        [TestMethod]
+        public void TestStringConverter()
+        {
+            var converter = new FeedbackMessageStringConverter();
+
+            Assert.IsNotNull(converter.StringFactory);
+
+            var message = FeedbackMessage.Info("Convert test");
+
+            Assert.AreEqual(converter.Convert(message), "Convert test");
+
+
+            converter.StringFactory = msg => "prefix-" + msg.ToString();
+
+            Assert.AreEqual(converter.Convert(message), "prefix-Convert test");
+        }
+    }
+}
