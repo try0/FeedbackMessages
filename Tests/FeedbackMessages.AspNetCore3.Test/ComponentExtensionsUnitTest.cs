9178892f84b77572f1c0c46d2570cb8713791b93
Ryo Tsunoda <try0.development@gmail.com>
2020-04-26 23:43:23 +0900

    Add Blazor test. (aspnet3.1 test project)

---
+++ Tests/FeedbackMessages.AspNetCore3.Test/ComponentExtensionsUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using FeedbackMessages.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static FeedbackMessages.FeedbackMessage;
+
+namespace FeedbackMessages.Test
+{
+
+    public class MockComponent : ComponentBase { 
+    }
+
+    [TestClass]
+    public class ComponentExtensionsUnitTest : FeedbackMessagesUnitTestBase
+    {
+
+        [TestMethod]
+        public void TestAddMessages()
+        {
+
+            InitializeHttpContext();
+
+            var component = new MockComponent();
+
+            component.InfoMessage("情報メッセージ1");
+            component.InfoMessage("情報メッセージ2");
+
+            component.SuccessMessage("success");
+            component.WarnMessage("warn");
+            component.ErrorMessage("error");
+
+
+            var store = FeedbackMessageStore.Current;
+            Assert.AreEqual(store.Count, 5);
+            Assert.AreEqual(store.GetFeedbackMessages(FeedbackMessageLevel.INFO).Count, 2);
+            Assert.AreEqual(store.GetFeedbackMessages(FeedbackMessageLevel.SUCCESS).Count, 1);
+            Assert.AreEqual(store.GetFeedbackMessages(FeedbackMessageLevel.WARN).Count, 1);
+            Assert.AreEqual(store.GetFeedbackMessages(FeedbackMessageLevel.ERROR).Count, 1);
+
+        }
+    }
+}
---
+++ Tests/FeedbackMessages.AspNetCore3.Test/FeedbackMessageScriptUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using FeedbackMessages.Components;
+using FeedbackMessages.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static FeedbackMessages.FeedbackMessage;
+
+namespace FeedbackMessages.Test
+{
+
+    //[TestClass] TODO
+    public class FeedbackMessageScriptUnitTest : FeedbackMessagesUnitTestBase
+    {
+
+    }
+}
