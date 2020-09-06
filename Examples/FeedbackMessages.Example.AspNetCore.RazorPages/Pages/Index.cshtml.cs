a4df62ef9bd67fb21ae3e4b9f26d08def3da4884
Ryo Tsunoda <try0.development@gmail.com>
2020-09-06 19:43:24 +0900

    Add examples to show validation errors. Razor Pages

--- Examples/FeedbackMessages.Example.AspNetCore.RazorPages/Pages/Index.cshtml.cs
+++ Examples/FeedbackMessages.Example.AspNetCore.RazorPages/Pages/Index.cshtml.cs
@@ -1,16 +1,33 @@
 ﻿using FeedbackMessages.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FeedbackMessages.Example.AspNetCore.RazorPages.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        [Required]
+        public string Message { get; set; }
+
         public void OnGet()
         {
             this.InfoMessage("Information message.");
             this.SuccessMessage("Success message.");
             this.WarnMessage("Warning message.");
             this.ErrorMessage("Error message.");
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                this.SuccessMessage("OK");
+            }
+
+            return Page();
         }
     }
 }
